6565e1fdb9d99efaac10f4a793df3448ae1dd94b
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2024-10-31 23:07:55 +0530

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-blue-500 font-semibold"></h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
